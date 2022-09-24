30e41b9a0580289bd988abdeba6338b0616f2daf
coderadu <47945947+coderadu@users.noreply.github.com>
2022-09-24 12:17:40 +0300

    fix 1

--- nvm/install.sh
+++ nvm/install.sh
@@ -4,5 +4,5 @@ if [ ! -d /data/packages/nvm ]; then
   mkdir -p /data/packages/nvm
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | NVM_DIR=/data/packages/nvm bash
   echo "export NVM_DIR=\"/data/packages/nvm\"" >> /config/ubuntu/onstart.sh
-  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"  # This loads nvm" >> /config/ubuntu/onstart.sh
+  echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm" >> /config/ubuntu/onstart.sh
 fi
