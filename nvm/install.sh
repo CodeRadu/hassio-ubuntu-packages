6bd5a98a561abde8e0fa33754e68c4aed17db548
coderadu <47945947+coderadu@users.noreply.github.com>
2022-09-24 12:21:11 +0300

    already installed message

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,8 +1,11 @@
-#! /usr/bin/bash
+#! /usr/bin/with-contenv bashio
+# shellcheck shell=bash
 
 if [ ! -d /data/packages/nvm ]; then
   mkdir -p /data/packages/nvm
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | NVM_DIR=/data/packages/nvm bash
   echo "export NVM_DIR=\"/data/packages/nvm\"" >> /config/ubuntu/onstart.sh
   echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm" >> /config/ubuntu/onstart.sh
+else
+  bashio::log.info "NVM already installed. Nothing to do"
 fi
